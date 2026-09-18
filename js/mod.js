4de72a6b64006415b0fd6cb9376fdd523056ad77
PeerInfinity <163462+PeerInfinity@users.noreply.github.com>
2026-09-17 19:19:48 -0700

    Squashed 'games/the-fysc-tree/' content from commit 298048c99
    git-subtree-dir: games/the-fysc-tree
    git-subtree-split: 298048c992cff48dd4b268ef2a19c0d99ba996bc

---
+++ js/mod.js
@@ -0,0 +1,88 @@
+
+
+let modInfo = {
+	name: "The FYSC Tree",
+	author: "liquidcashews",
+	pointsName: "fysc players",
+	modFiles: ["layers.js", "tree.js"],
+
+	discordName: "FYSC Discord",
+	discordLink: "https://discord.gg/gNtmshDX9z",
+	initialStartPoints: new Decimal (0), // Used for hard resets and new players
+	offlineLimit: 1,  // In hours
+}
+
+// Set your version in num and name
+let VERSION = {
+	num: "1.22",
+	name: "Quality of Inflation",
+}
+
+let changelog = `<h1>Changelog:</h1><br>,
+	<h3>v0.01<h3><br>
+	- Idk anything about CSS so no changelog for you`
+let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
+
+// If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
+// (The ones here are examples, all official functions are already taken care of)
+var doNotCallTheseFunctionsEveryTick = ["blowUpEverything"]
+
+function getStartPoints(){
+    return new Decimal(modInfo.initialStartPoints)
+}
+
+// Determines if it should show points/sec
+function canGenPoints(){
+	return true
+}
+
+// Calculate points/sec!
+function getPointGen() {
+	if(!canGenPoints())
+		return new Decimal(0)
+
+	let gain = new Decimal(1)
+	if (hasUpgrade("subs", 11)) gain = gain.times(2.5)
+	if (hasUpgrade("subs", 12)) gain = gain.times(upgradeEffect("subs", 12))
+	if (hasUpgrade("subs", 13)) gain = gain.times(upgradeEffect("subs", 13))
+    if (hasUpgrade("hexzd", 11)) gain = gain.times(3)
+	if (hasUpgrade("hexzd", 12)) gain = gain.times(upgradeEffect("hexzd", 12))
+	if (hasUpgrade("hexzd", 25)) gain = gain.times(4)
+	if (hasUpgrade("subs", 23)) gain = gain.pow(1.1)
+	if (hasMilestone("ct", 0)) gain = gain.times(5)
+	gain = gain.times(new Decimal (1.2).pow(player.ct.points))
+    if (inChallenge("hexzd", 11)) gain = gain.pow(0.5)
+	return gain
+}
+
+// You can add non-layer related variables that should to into "player" and be saved here, along with default values
+function addedPlayerData() { return {
+}}
+
+// Display extra things at the top of the page
+var displayThings = [
+]
+
+// Determines when the game "ends"
+function isEndgame() {
+	return player.points.gte(new Decimal("e1000000000"))
+}
+
+
+
+// Less important things beyond this point!
+
+// Style for the background, can be a function
+var backgroundStyle = {
+
+}
+
+// You can change this if you have things that can be messed up by long tick lengths
+function maxTickLength() {
+	return(3600) // Default is 1 hour which is just arbitrarily large
+}
+
+// Use this if you need to undo inflation from an older version. If the version is older than the version that fixed the issue,
+// you can cap their current resources with this.
+function fixOldSave(oldVersion){
+}
