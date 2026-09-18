7923b7936debf7caa25f359298aa35cc76dfbbfc
PeerInfinity <163462+PeerInfinity@users.noreply.github.com>
2026-09-17 18:09:51 -0700

    Squashed 'games/the-greek-tree/' content from commit 1966cf8dc
    git-subtree-dir: games/the-greek-tree
    git-subtree-split: 1966cf8dc8b58080c7d892b4312ac3008dca2691

---
+++ js/mod.js
@@ -0,0 +1,134 @@
+let modInfo = {
+	name: "The Greek Tree",
+	author: "nobody",
+	pointsName: "points",
+	modFiles: ["layers.js", "side_layers.js", "tree.js"],
+
+	discordName: "",
+	discordLink: "",
+	initialStartPoints: new Decimal (10), // Used for hard resets and new players
+	offlineLimit: 0,  // In hours
+}
+
+// Set your version in num and name
+let VERSION = {
+	num: "0.1",
+	name: "Starting !",
+}
+
+let changelog = `<h1>Changelog:</h1><br>
+    <h3>v0.1: Release !</h3><br>
+	- <b>Added <span style="color:#ff0000">alpha layer</span></b><br>
+    - <small>Added 25 Upgrades.<br>
+	- Added 6 Buyables.<br></small>
+	- <b>Added <span style="color:#1663f1">beta layer</span></b><br>
+    - <small>Added 25 Upgrades.<br>
+	- Added 6 Milestones.<br>
+	- Added 6 Buyables.<br></small>
+	- <b>Added <span style="color:#fc8600">gamma layer</span></b><br>
+    - <small>Added 25 Upgrades.<br>
+	- Added 7 Milestones.<br></small>
+	- <b>Added <span style="color:#ac00fc">delta layer</span></b><br>
+    - <small>Added a new currency.<br>
+	- Added 12 Upgrades.<br>
+	- Added 6 Milestones.<br>
+	- Added 2 Buyables.<br></small>
+	<b>Added <span style="color:#FFFF00">achievements</span></b><br>
+	- <small>Added 23 achievements <br></small>
+	<b>Added <span style="color:#FFFFFF">statistic layer</span></b><br>`
+
+
+let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
+
+// If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
+// (The ones here are examples, all official functions are already taken care of)
+var doNotCallTheseFunctionsEveryTick = ["blowUpEverything"]
+
+function getStartPoints(){
+    return new Decimal(modInfo.initialStartPoints)
+}
+
+// Determines if it should show points/sec
+function canGenPoints(){
+	return true
+}
+
+// Calculate points/sec!
+function getPointGen() {
+	if(!canGenPoints())
+		return new Decimal(0)
+
+	let gain = new Decimal(1)
+	if (hasUpgrade('A',11)) gain = gain.times(2)
+	if (hasUpgrade('A',12)) gain = gain.times(2)
+	if (hasBuyable('A',11)) gain = gain.times(buyableEffect('A',11))
+	if (hasUpgrade('A',23)) gain = gain.times(upgradeEffect('A',23))
+	if (hasUpgrade('A',25)) gain = gain.times(upgradeEffect('A',25))
+	if (hasUpgrade('A',26)) gain = gain.times(upgradeEffect('A',26))
+	if (hasUpgrade('A',31)) gain = gain.times(upgradeEffect('A',31))
+	if (hasUpgrade('A',34)) gain = gain.times(34)
+
+	if (hasUpgrade('B',11)) gain = gain.times(upgradeEffect('B',11))
+	if (hasUpgrade('B',12)) gain = gain.times(5)
+	if (hasUpgrade('B',13)) gain = gain.times(10)
+	if (hasUpgrade('B',15)) gain = gain.times(upgradeEffect('B',15))
+	if (hasMilestone('B',3)) gain = gain.times(1e10)
+	if (hasUpgrade('B',44)) gain = gain.times(getBuyableAmount('A',11).pow(4)).max(1)
+	if (hasUpgrade('B',51)) gain = gain.times(upgradeEffect('B',51))
+
+	if (hasUpgrade('G',11)) gain = gain.times(4)
+	if (hasUpgrade('G',12)) gain = gain.times(1e15)
+	if (hasUpgrade('G',13)) gain = gain.times(player.G.total.pow(32))
+	if (hasUpgrade('G',25)) gain = gain.times(upgradeEffect('G',25))
+	if (hasUpgrade('G',55)) gain = gain.times(upgradeEffect('G',55))
+
+	if (hasUpgrade('D',12)) gain = gain.times(upgradeEffect('D',12))
+
+	if (hasAchievement('ach',23)) gain = gain.times(23)
+
+	if (player.D.dSeed.gte(1)) gain = gain.times(player.D.dSeed.pow(50000))
+	// Apply point gain exponent (example: upgrades/buyables can increase this)
+	let pointExp = new Decimal(1)
+	try {
+		if (hasUpgrade('B',31)) pointExp = pointExp.add(new Decimal(0.15))
+		if (hasMilestone('G',0)) pointExp = pointExp.add(milestoneEffect('G',0))
+		if (hasBuyable('B',22)) pointExp = pointExp.add(buyableEffect('B',22))
+		if (hasBuyable('B',23)) pointExp = pointExp.add(buyableEffect('B',23))
+	} catch (e) {}
+	// Raise the per-second gain to the computed exponent
+	gain = gain.pow(pointExp)
+	return gain
+}
+
+// You can add non-layer related variables that should to into "player" and be saved here, along with default values
+function addedPlayerData() { return {
+}}
+
+// Display extra things at the top of the page
+var displayThings = [
+	'Endgame: ~1e413.5k,'
+]
+
+// Determines when the game "ends"
+function isEndgame() {
+	return player.points.gte(new Decimal("e413500"))
+}
+
+
+
+// Less important things beyond this point!
+
+// Style for the background, can be a function
+var backgroundStyle = {
+
+}
+
+// You can change this if you have things that can be messed up by long tick lengths
+function maxTickLength() {
+	return(3600) // Default is 1 hour which is just arbitrarily large
+}
+
+// Use this if you need to undo inflation from an older version. If the version is older than the version that fixed the issue,
+// you can cap their current resources with this.
+function fixOldSave(oldVersion){
+}
---
+++ js/utils/buyable_file.js
@@ -0,0 +1,146 @@
+function buyableCostFormula(layer, id) {
+    x = getBuyableAmount(layer, id)
+
+    if (layer == 'A') {
+        if (id == '11') return "1.015^(x^1.5)"
+        else if (id == '12') {
+             let val = "1.25^(x^1.5)" 
+            if (hasUpgrade('A',53)) val = "1.25^(x^1.25)"
+            return val
+        }
+        else if (id == '13') {
+            let val = "2^(x^1.5)" 
+            if (hasUpgrade('A',45)) val = "2^(x^1.38)"
+            return val
+        }
+        else if (id == '21') return "1.1^(x^2)"
+        else if (id == '22') return "1.25^(x^2)"
+        else if (id == '23') {
+            let val = "2^(x^2.5)" 
+            if (hasUpgrade('A',55)) val = "2^(x^2.25)"
+            return val
+        }
+    } else if (layer == 'B'){
+        if (id == '11') { 
+            let val = "1.2^(x^1.5)"
+            if (hasUpgrade('D',16)) val = "1.05^(x^1.5)"
+            return val}
+        else if (id == '12') {
+            let power = new Decimal("2")
+            let val = "1.75^(x^"+power+")" 
+            if (hasMilestone('G',2)) power = power.sub(milestoneEffect('G',2)) ; val = "1.75^(x^"+power+")"
+            if (hasUpgrade('G',35)) val = "1.5^(x^"+power+")"
+            return val
+        }
+        else if (id == '13') return "1.025*1e5^(x^2)"
+        else if (id == '21') return "1.035*1e12^(x^1.5)"
+        else if (id == '22') {
+            let val = "1.055*1e25^(x^2)"
+            if (hasUpgrade("G",34)) val = "1.055*1e22^(x^2)"
+            return val
+        }
+        else if (id == '23') return "base*1.075*1e30^(x^1.7)"
+    } else if (layer == 'D'){
+        if (id == '11') return "base*1.01^x²"
+        else if (id == '12') return "base*1.12^x²"
+        else if (id == '13') {
+            let val = "base*2.5^x^2.25"
+            if (hasUpgrade('D',212)) val = "base*2.5^x^2.1"
+            return val
+        }
+        else if (id == '14') return "base*5^x^2.25"
+    }
+}
+
+function buyableBulk(layer){
+    let amt = new Decimal(1)
+
+    if (layer == 'A'){
+        if (hasMilestone('B', 4)) amt = amt.times(10)
+        if (hasMilestone('B', 5)) amt = amt.times(3)
+        if (hasMilestone('G', 2)) amt = amt.times(3)
+        if (hasMilestone('G', 5)) amt = amt.times(10)
+    }
+    if (layer == 'B'){
+        if (hasMilestone('G', 5)) amt = amt.times(10)
+        if (hasMilestone('D', 0)) amt = amt.times(5)
+        if (hasMilestone('D',7)) amt = amt.times(5)
+    }
+    return amt
+}
+
+function getAutoBuySpeed(layer){
+    let mult = new Decimal(1)
+    if (layer == "A") {
+        if (hasUpgrade('B', 33)) mult = mult.times(2)
+        if (hasUpgrade('G', 11)) mult = mult.times(10)
+        if (hasMilestone('G', 5)) mult = mult.times(5)
+        if (hasMilestone('D', 0)) mult = mult.times(player.D.resets.add(1))
+    }
+    if (layer == "B") {
+        if (hasMilestone('G', 2)) mult = mult.times(3)
+        if (hasMilestone('G', 5)) mult = mult.times(5)
+    }
+    return mult
+}
+
+function buyAllAtOnce(layer, bulk = new Decimal(1)) {
+    if (!player[layer].buyables) return
+    for (let id in layers[layer].buyables) {
+        if (!isNaN(id)) {
+            const buyable = layers[layer].buyables[id]
+            if (buyable.canAfford?.()) {
+                let remaining = new Decimal(bulk)
+                while (remaining.gte(1) && buyable.canAfford?.()) {
+                    buyable.buy()
+                    remaining = remaining.sub(1)
+                }
+            }
+        }
+    }
+}
+
+function generalizedBuyableLogic(diff, layer, condition) {
+    if (!player[layer].buyables) return
+
+    if (!player[layer].buyableTime) player[layer].buyableTime = new Decimal(0)
+    player[layer].buyableTime = player[layer].buyableTime.add(diff)
+
+    const speed = getAutoBuySpeed(layer).max(1)
+    const tickInterval = new Decimal(1).div(speed)
+    const bulk = buyableBulk(layer)
+
+    const ids = Object.keys(layers[layer].buyables)
+        .filter(id => !isNaN(id))
+        .sort((a, b) => Number(a) - Number(b))
+
+    while (player[layer].buyableTime.gte(tickInterval)) {
+        player[layer].buyableTime = player[layer].buyableTime.sub(tickInterval)
+
+        if (condition) {
+            buyAllAtOnce(layer, bulk)
+        } else {
+            for (let id of ids) {
+                const buyable = layers[layer].buyables[id]
+                if (buyable.canAfford?.()) {
+                    let remaining = new Decimal(bulk)
+                    while (remaining.gte(1) && buyable.canAfford?.()) {
+                        buyable.buy()
+                        remaining = remaining.sub(1)
+                    }
+                    break
+                }
+            }
+        }
+    }
+}
+
+function safeBuy(layer, cost, currency = "points") {
+    let curr = new Decimal(player[layer][currency])
+    if (!curr.gte(cost)) return false
+    player[layer][currency] = curr.sub(cost)
+    if (player[layer][currency].lt(0) || Decimal.isNaN(player[layer][currency])) {
+        player[layer][currency] = decimalZero
+    }
+    return true
+}
